45e4a7e43e5521a4bd9a758a6350a91ac86e0c90
Calvin Durrant <Calvin.Durrant@standardbank.co.za>
2022-02-26 22:55:21 +0200

    change detail view route

--- libs/fakestore/feature-cart/src/index.ts
+++ libs/fakestore/feature-cart/src/index.ts
@@ -2,3 +2,4 @@ export * from './lib/cart.module';
 export * from './lib/+state/cart.selectors';
 export * from './lib/+state/cart.reducer';
 export * from './lib/+state/cart.actions';
+export * from './lib/+state/cart.facade';
