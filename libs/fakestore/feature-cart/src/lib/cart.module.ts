07a6847b1f7a9362d5200bf74321757ab04dcc1f
Calvin Durrant <Calvin.Durrant@standardbank.co.za>
2022-02-28 14:21:44 +0200

    remove unused cart effects

--- libs/fakestore/feature-cart/src/lib/cart.module.ts
+++ libs/fakestore/feature-cart/src/lib/cart.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StoreModule } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
 import * as fromCart from './+state/cart.reducer';
-import { CartEffects } from './+state/cart.effects';
 import { CartComponent } from './cart.component';
 import { CartItemModule } from '@fakestore/ui/cart';
 import { RouterModule } from '@angular/router';
@@ -15,7 +13,6 @@ import { RouterModule } from '@angular/router';
   imports: [
     CommonModule,
     StoreModule.forFeature(fromCart.CART_FEATURE_KEY, fromCart.reducer),
-    EffectsModule.forFeature([CartEffects]),
     CartItemModule,
     RouterModule,
   ],
